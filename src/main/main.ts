26c9bdb8d37bc7274ea8da579456b4028cbf842b
Alex Leung <alexleung25@outlook.com>
2022-09-06 23:14:06 -0700

    Updated main

--- src/main/main.ts
+++ src/main/main.ts
@@ -2,8 +2,11 @@ import '../styles/main.css'
 import { selection } from '../selection/selection'
 import { editor } from '../editor/editor'
 
-//call selection function for inserting dropdown selections to the DOM  
-selection();
+function main(): void {
+    //call selection function for inserting dropdown selections to the DOM  
+    selection();
 
-//call editor
-editor();
+    //call editor
+    editor();
+}
+main();
